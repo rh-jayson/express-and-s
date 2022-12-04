fbabecdb49f12d187f7748398c6c103fcf9fedaf
Jayson Jorgensen <jayson.jorgensen@readinghorizons.com>
2022-12-04 11:40:27 -0700

    moving some more stuff to env

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
 
 const pgp = require("pg-promise")(/* options */);
 const db = pgp(process.env.CONNECTION_STRING);
 
 app.use(function (req, res, next) {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://sounds-and-sights.onrender.com"
-  );
+  res.setHeader("Access-Control-Allow-Origin", process.env.CORS_URL);
   res.setHeader("Access-Control-Allow-Methods", "GET");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -18,17 +14,11 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get("/", (req, res) => {
-  try {
-    console.log("you successfully hit the / route");
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
 app.get("/directors", async (req, res) => {
   try {
-    const directors = await db.any("SELECT * FROM public.director");
+    const directors = await db.any(
+      "SELECT * FROM public.director ORDER BY family_name ASC"
+    );
     res.status(200).send(directors);
   } catch (error) {
     res.status(500).send(error);
@@ -37,11 +27,20 @@ app.get("/directors", async (req, res) => {
 
 app.get("/films", async (req, res) => {
   try {
-    const films = await db.any("SELECT * FROM film");
+    const films = await db.any("SELECT * FROM film ORDER BY title ASC");
     res.status(200).send(films);
   } catch (error) {
     res.status(500).send(error);
   }
 });
 
-app.listen(port, () => console.log(`S&S app listening on port ${port}!`));
+app.get("/countries", async (req, res) => {
+  try {
+    const films = await db.any("SELECT * FROM country");
+    res.status(200).send(films);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = app;
